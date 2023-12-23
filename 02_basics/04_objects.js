d17dbcee8a0c6c45d0d67fd452e29cc8b8bcdabf
Prabal Patidar <108178339+PrabalPatidar@users.noreply.github.com>
2023-12-23 23:51:07 +0530

    Add files via upload

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -48,10 +48,66 @@ const users = [
 ]
 
 users[1].email
-console.log(instaUser);
+// console.log(instaUser);
 
-console.log(Object.keys(instaUser));
-console.log(Object.values(instaUser));
-console.log(Object.entries(instaUser));     // har ek key value ko array me bana diya jata  hai  1st property is key and 2nd is value
+// console.log(Object.keys(instaUser));
+// console.log(Object.values(instaUser));
+// console.log(Object.entries(instaUser));     // har ek key value ko array me bana diya jata  hai  1st property is key and 2nd is value
 
-console.log(instaUser.hasOwnProperty('isLoggedIn'));      // for check the property is available or not
+// console.log(instaUser.hasOwnProperty('isLoggedIn'));      // for check the property is available or not
+
+
+// ************ gddd ******
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+/*    this is react example
+const navbar = () => {              // this is react it is example that we use  const navbar = (props.company *this is use in react *) => {}
+                                    // we can use that formet const navbar = ({company}) => { }   // this is best practice
+}
+
+navbar(company = "hitesh")
+*/
+/*
+this is json
+{
+
+}
+
+//aese nhi likhna hai ye object nhi hai sirf obj ka naam hota hai almost json ki tarah treat kiya jata hai
+{
+    name: "hitesh",                      
+    coursename: "js in hindi",           
+    price: "free"
+ }
+
+ // proper json structure means usually keys bhi ek string hoti hai or uski value bhi string hoti
+{                                           
+    "name": "hitesh",
+    "coursename": "js in hindi",
+    "price": "free"
+  }
+*/
+  {                                           
+    "name": "hitesh",
+    "coursename": "js in hindi",
+    "price": "free"
+  }
+
+  // api in array formet
+  [
+    {},
+    {},
+    {}
+  ]
